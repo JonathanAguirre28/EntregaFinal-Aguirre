10193d4368173332b517dfa7331f0a79b8659c2e
JonathanAguirre <jonathan621924@gmail.com>
2023-08-02 16:27:18 -0300

    creando mi formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardModule } from './dashboard/dashboard.module';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule,
+    BrowserAnimationsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/pages/users/users.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent {
+  nameControl = new FormControl(null, [Validators.required])
+  surnameControl = new FormControl(null, [Validators.required])
+  emailControl = new FormControl(null, [Validators.required])
+  passwordControl = new FormControl(null, [Validators.required])
+
+
+  userForm = new FormGroup({
+     name: this.nameControl,
+     surname: this.surnameControl,
+     email: this.emailControl,
+     password: this.passwordControl,
+  });
+
+  onSubmit(): void {
+    alert(JSON.stringify(this.userForm.value));
+  }
+}
